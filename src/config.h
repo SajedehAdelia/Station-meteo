95e90a8c116685420a57b397eab6b80877b5b278
Maxence COEUR <maxence.coeur@etu.univ-lyon1.fr>
2025-02-24 14:22:25 +0100

    ca marche luc + temp + hum

--- src/config.h
+++ src/config.h
@@ -6,15 +6,31 @@
 #define TOPIC_ROOT "StationMeteoAdeliaLoanGaelMaxence"
 #define TOPIC_LOCALISATION "terrasse"
 #define TOPIC_SENSOR_LUMINOSITE "luminosite"
-#define TOPIC_ALERTE "luminosite/alerte"
-#define TOPIC_ERREUR "luminosite/erreur"
+#define TOPIC_SENSOR_THERMOMETRE "thermometre"
+#define TOPIC_SENSOR_HUMIDITE "humidite"
+#define TOPIC_ALERTE "alerte"
+#define TOPIC_ERREUR "erreur"
 
 #define ERREUR_BRANCHEMENT "Le capteur est mal branché"
 #define ALERTE_MAX_LUMINOSITE "luminosite > max"
 #define ALERTE_MIN_LUMINOSITE "luminosite < min"
+#define ALERTE_MAX_TEMPERATURE "temperature > max"
+#define ALERTE_MIN_TEMPERATURE "temperature < min"
+#define ALERTE_MAX_HUMIDITE "humidite > max"
+#define ALERTE_MIN_HUMIDITE "humidite < min"
 
 const int LUMINOSITE_MAX_ALERTE = 1000;
 const int LUMINOSITE_MIN_ALERTE = 10;
 
-#define PIN_LED 2
+const int TEMPERATURE_MAX_ALERTE = 35;
+const int TEMPERATURE_MIN_ALERTE = -10;
+
+const int HUMIDITE_MAX_ALERTE = 60;
+const int HUMIDITE_MIN_ALERTE = 40;
+
+#define PIN_LED LED_BUILTIN
 #define ADRESS 0x77
+
+
+#define DHTTYPE DHT11
+#define DHTPIN D0
--- src/sensor/sensor.cpp
+++ src/sensor/sensor.cpp
@@ -6,13 +6,17 @@
 #include "../mqtt_client/mqtt_client.h"
 #include "config.h"
 #include <BH1750.h>
+#include <DHT.h>
 
 // Adafruit_BMP085 bmp;
 BH1750 lightMeter;
+DHT dht(DHTPIN, DHTTYPE);
 
 void initSensors() {
     Wire.begin();
     lightMeter.begin();
+
+    dht.begin();
 
     Serial.println(F("BH1750 Test begin"));
 //     if (!bmp.begin(BMP085_STANDARD)) {
@@ -26,14 +30,49 @@ void initSensors() {
 void readAndSendSensorData() {
     float lux = lightMeter.readLightLevel();
     if (isnan(lux)) {
-        publishMessage(TOPIC_ERREUR, ERREUR_BRANCHEMENT);
+        std::string topic = std::string(TOPIC_SENSOR_LUMINOSITE) + "/" + TOPIC_ERREUR;
+        publishMessage(topic.c_str(), ERREUR_BRANCHEMENT);
     } else {
         publishMessage(TOPIC_SENSOR_LUMINOSITE, String(lux).c_str());
         
         if (lux < LUMINOSITE_MIN_ALERTE) {
-            publishMessage(TOPIC_ALERTE, ALERTE_MIN_LUMINOSITE);
+            std::string topic = std::string(TOPIC_SENSOR_LUMINOSITE) + "/" + TOPIC_ALERTE;
+            publishMessage(topic.c_str(), ALERTE_MIN_LUMINOSITE);
         } else if (lux > LUMINOSITE_MAX_ALERTE) {
-            publishMessage(TOPIC_ALERTE, ALERTE_MAX_LUMINOSITE);
+            std::string topic = std::string(TOPIC_SENSOR_LUMINOSITE) + "/" + TOPIC_ALERTE;
+            publishMessage(topic.c_str(), ALERTE_MAX_LUMINOSITE);
+        }
+    }
+
+    float temperature = dht.readTemperature();
+    float humidite = dht.readHumidity();
+
+    if (std::isnan(temperature)) {
+        std::string topic = std::string(TOPIC_SENSOR_THERMOMETRE) + "/" + TOPIC_ERREUR;
+        publishMessage(topic.c_str(), ERREUR_BRANCHEMENT);
+    }
+    if (std::isnan(humidite)) {
+        std::string topic = std::string(TOPIC_SENSOR_HUMIDITE) + "/" + TOPIC_ERREUR;
+        publishMessage(topic.c_str(), ERREUR_BRANCHEMENT);
+    }
+    else {
+        publishMessage(TOPIC_SENSOR_THERMOMETRE, String(temperature).c_str());
+        publishMessage(TOPIC_SENSOR_HUMIDITE, String(humidite).c_str());
+
+        if (temperature < TEMPERATURE_MIN_ALERTE) {
+            std::string topic = std::string(TOPIC_SENSOR_THERMOMETRE) + "/" + TOPIC_ALERTE;
+            publishMessage(topic.c_str(), ALERTE_MIN_TEMPERATURE);
+        } else if (temperature > TEMPERATURE_MAX_ALERTE) {
+            std::string topic = std::string(TOPIC_SENSOR_THERMOMETRE) + "/" + TOPIC_ALERTE;
+            publishMessage(topic.c_str(), ALERTE_MAX_TEMPERATURE);
+        }
+
+        if (humidite < HUMIDITE_MIN_ALERTE) {
+            std::string topic = std::string(TOPIC_SENSOR_HUMIDITE) + "/" + TOPIC_ALERTE;
+            publishMessage(topic.c_str(), ALERTE_MIN_HUMIDITE);
+        } else if (humidite > HUMIDITE_MAX_ALERTE) {
+            std::string topic = std::string(TOPIC_SENSOR_HUMIDITE) + "/" + TOPIC_ALERTE;
+            publishMessage(topic.c_str(), ALERTE_MAX_HUMIDITE);
         }
     }
 }
